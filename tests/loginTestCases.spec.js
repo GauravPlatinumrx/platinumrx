af0f668bf95c4705ece103ee319b130a8a41f581
Gaurav Sharma <sankhyan.gaurav2000@gmail.com>
2024-08-02 18:29:59 +0530

    login Test cases file addes

---
+++ tests/loginTestCases.spec.js
@@ -0,0 +1,137 @@
+import { test, expect } from '@playwright/test';
+test('test1', async ({ page }) => {
+// Open WebSite    
+await page.goto('https://flexipill-ui-new-staging.vercel.app')
+// Verify the URL is same
+await expect(page).toHaveURL('https://flexipill-ui-new-staging.vercel.app');
+//click on login buttton
+await page.getByRole('link', { name: 'Login' }).click();
+
+ // For entering Number
+ const numberInput = page.getByPlaceholder('Enter your number', { exact: true });
+  await expect(numberInput).toBeVisible();
+  await numberInput.fill('1111111111');
+
+  // Continue Button
+  const continueButton = page.getByRole('button', { name: 'Continue' });
+   await expect(continueButton).toBeVisible();
+   await continueButton.click()
+// For Entering OTP
+await page.getByRole('textbox').first().click();
+await page.getByRole('textbox').first().fill('1');
+await page.getByRole('textbox').nth(1).fill('1');
+await page.getByRole('textbox').nth(2).fill('1');
+await page.getByRole('textbox').nth(3).fill('1');
+
+// Verify that the OTP was entered correctly
+await expect(page.getByRole('textbox').first()).toHaveValue('1');
+await expect(page.getByRole('textbox').nth(1)).toHaveValue('1');
+await expect(page.getByRole('textbox').nth(2)).toHaveValue('1');
+await expect(page.getByRole('textbox').nth(3)).toHaveValue('1');
+
+
+});
+
+test.only('test2', async ({ page }) => {
+// Open WebSite    
+await page.goto('https://flexipill-ui-new-staging.vercel.app')
+
+await expect(page).toHaveURL('https://flexipill-ui-new-staging.vercel.app');
+//click on login buttton
+await page.getByRole('link', { name: 'Login' }).click();
+
+ // For entering Number
+
+  // For entering Number
+  const numberInput = page.getByPlaceholder('Enter your number', { exact: true });
+  await expect(numberInput).toBeVisible();
+  await numberInput.fill('111111111');
+
+  // Continue Button
+  const continueButton = page.getByRole('button', { name: 'Continue' });
+   await expect(continueButton).toBeVisible();
+   await continueButton.click()
+// For Entering OTP
+await page.getByRole('textbox').first().click();
+await page.getByRole('textbox').first().fill('1');
+await page.getByRole('textbox').nth(1).fill('1');
+await page.getByRole('textbox').nth(2).fill('1');
+await page.getByRole('textbox').nth(3).fill('1');
+
+// Verify that the OTP was entered correctly
+await expect(page.getByRole('textbox').first()).toHaveValue('1');
+await expect(page.getByRole('textbox').nth(1)).toHaveValue('1');
+await expect(page.getByRole('textbox').nth(2)).toHaveValue('1');
+await expect(page.getByRole('textbox').nth(3)).toHaveValue('1');
+
+});
+
+
+test.only('test3', async ({ page }) => {
+  // Open WebSite    
+  await page.goto('https://flexipill-ui-new-staging.vercel.app')
+
+  await expect(page).toHaveURL('https://flexipill-ui-new-staging.vercel.app');
+  //click on login buttton
+  await page.getByRole('link', { name: 'Login' }).click();
+  
+   // For entering Number
+
+    // For entering Number
+    const numberInput = page.getByPlaceholder('Enter your number', { exact: true });
+    await expect(numberInput).toBeVisible();
+    await numberInput.fill('111111  111');
+
+    // Continue Button
+    const continueButton = page.getByRole('button', { name: 'Continue' });
+     await expect(continueButton).toBeVisible();
+     await continueButton.click()
+  // For Entering OTP
+  await page.getByRole('textbox').first().click();
+  await page.getByRole('textbox').first().fill('1');
+  await page.getByRole('textbox').nth(1).fill('1');
+  await page.getByRole('textbox').nth(2).fill('1');
+  await page.getByRole('textbox').nth(3).fill('1');
+
+  // Verify that the OTP was entered correctly
+await expect(page.getByRole('textbox').first()).toHaveValue('1');
+await expect(page.getByRole('textbox').nth(1)).toHaveValue('1');
+await expect(page.getByRole('textbox').nth(2)).toHaveValue('1');
+await expect(page.getByRole('textbox').nth(3)).toHaveValue('1');
+  
+  });
+
+
+  test.only('test4', async ({ page }) => {
+    // Open WebSite    
+    await page.goto('https://flexipill-ui-new-staging.vercel.app')
+
+    await expect(page).toHaveURL('https://flexipill-ui-new-staging.vercel.app');
+    //click on login buttton
+    await page.getByRole('link', { name: 'Login' }).click();
+    
+     // For entering Number
+
+      // For entering Number
+      const numberInput = page.getByPlaceholder('Enter your number', { exact: true });
+      await expect(numberInput).toBeVisible();
+      await numberInput.fill('111111acx111');
+
+      // Continue Button
+      const continueButton = page.getByRole('button', { name: 'Continue' });
+       await expect(continueButton).toBeVisible();
+       await continueButton.click()
+    // For Entering OTP
+    await page.getByRole('textbox').first().click();
+    await page.getByRole('textbox').first().fill('1');
+    await page.getByRole('textbox').nth(1).fill('1');
+    await page.getByRole('textbox').nth(2).fill('1');
+    await page.getByRole('textbox').nth(3).fill('1');
+
+    // Verify that the OTP was entered correctly
+  await expect(page.getByRole('textbox').first()).toHaveValue('1');
+  await expect(page.getByRole('textbox').nth(1)).toHaveValue('1');
+  await expect(page.getByRole('textbox').nth(2)).toHaveValue('1');
+  await expect(page.getByRole('textbox').nth(3)).toHaveValue('1');
+    
+    });
