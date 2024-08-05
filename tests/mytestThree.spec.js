db03d6d6440eb887c85dcd7cf6f166df43e8612a
Gaurav Sharma <sankhyan.gaurav2000@gmail.com>
2024-08-05 18:43:38 +0530

    random commit

--- tests/mytestThree.spec.js
+++ tests/mytestThree.spec.js
@@ -1,24 +1,4 @@
-// @ts-check
-const { test, expect } = require('@playwright/test');
-
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
-
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
-});
-
-
+import { test, expect } from '@playwright/test';
 
 test('test', async ({ page }) => {
   await page.goto('https://flexipill-ui-new-staging.vercel.app/');
@@ -36,11 +16,3 @@ test('test', async ({ page }) => {
   await page.locator('div').filter({ hasText: 'Add Images/docUpload Photo/PDF' }).nth(1).setInputFiles('Screenshot 2024-07-31 120443.png');
   await page.locator('.MuiBackdrop-root').click();
 });
-
-
-
-
-
-
-
-
